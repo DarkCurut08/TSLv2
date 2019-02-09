0ad21ea8395d9ab047d2beed29de33931c6e2c47
DarkCurut08 <42291700+DarkCurut08@users.noreply.github.com>
2019-02-09 07:25:54 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+pkg install python
+clear
+pkg install python2
+clear
+pip2 install lolcat
+clear
+pkg install figlet
+clear
+figlet -f slant ' Moreno77' | lolcat
+echo " [!] Tools By : Moreno77" | lolcat
+echo " [!] Youtube.com/c/DarkCurut08" | lolcat
+echo " [!] 085606753071 / 083113611267" | lolcat
+echo " [!] THX " | lolcat
